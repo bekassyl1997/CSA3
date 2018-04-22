57f8d2382fa39812aa639044bb01ffeae5c23979
bekassyl1997 <36645680+bekassyl1997@users.noreply.github.com>
2018-04-23 01:25:34 +0600

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,5 +1,9 @@
 import numpy as np
+import math
 
+a = np.nan
+b = np.nan
+content = np.full((5000,), np.nan)
 pc = 0
 bytes = {0x00: 1, 0x01: 1, 0x02: 1, 0x03: 2, 0x04: 2, 0x05: 3,
          0x06: 3, 0x07: 3, 0x08: 3, 0x09: 1, 0x0A: 1, 0x0B: 1,
@@ -68,28 +72,62 @@ def controlLogic():
 
 
 # TEST FUNCTIONS
-# Task 1
-def test1():
-    print('TEST: Task 1')
-    ram = storeRam(readHex())
-    for i in range(len(ram)):
-        print(format(ram[i], '02x'))
+# Final
+def finalFunction():
+    s = readHex()
+    ram = storeRam(s)
+    global a
+    global b
+    global content
+    print("A\tB")
+    while pc < len(ram):
+      #  print(content[pc])
+        offset = bytes[(ram[pc])]
+     #   print(format(ram[pc], '02x'), end=' ')
+        if offset == 3:
+            if ram[pc] == 0x05:
+                a = content[ram[pc + 1] * 0x100 + ram[pc + 2]]
+            elif ram[pc] == 0x06:
+                b = content[ram[pc + 1] * 0x100 + ram[pc + 2]]
+            elif ram[pc] == 0x07:
+                content[ram[pc + 1] * 0x100 + ram[pc + 2]] = a
+            elif ram[pc] == 0x08:
+                content[ram[pc + 1] * 0x100 + ram[pc + 2]] = b
+            elif ram[pc] == 0x17:
+                ram[pc + 3] = content[ram[pc + 1] * 0x100 + ram[pc + 2]]
+            elif ram[pc] == 0x18:
+                if a == 0x00:
+                    ram[pc + 3] = content[ram[pc + 1] * 0x100 + ram[pc + 2]]
+            elif ram[pc] == 0x19:
+                if a != 0x00:
+                    ram[pc + 3] = content[ram[pc + 1] * 0x100 + ram[pc + 2]]
 
-# Task 2
-def test2():
-    a, b = 0x3, 0x6
-    print('\nTEST: Task 2')
-    ram = storeRam(readHex())
-    print('Hex:\tAns:')
-    for i in range(len(ram)):
-        print(format(ram[i], '02x'), end='\t\t')
-        ans = alu(a, b, ram[i])
-        if ans == False:
-            print(False)
+            # print(format(ram[pc + 1], '02x'), end='')
+            # print(format(ram[pc + 2], '02x'), end=' ')
+        elif offset == 2:
+            if ram[pc] == 0x03:
+                a = ram[pc + 1]
+            elif ram[pc] == 0x04:
+                b = ram[pc + 1]
+        temp = alu(a, b, ram[pc])
+        if temp != False:
+            a = int(temp)
+
+        if math.isnan(a):
+            print("nan", end="\t")
         else:
-            print(format(ans, 'x'))
+            print(format(int(a), '02x'), end='\t')
 
-# Task 3, 4
-def test34():
-    print('\nTEST: Task 3, 4')
-    controlLogic()
+        if math.isnan(b):
+            print("nan")
+        else:
+            print(format(int(b), '02x'))
+
+        updatePC(offset)
+
+    for i in range(5000):
+        if math.isnan(content[i]) == False:
+            print("Address ", end=" ")
+            print(format(i, '04x'), end=": ")
+            print(format(int(content[i]), '02x'))
+
